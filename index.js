14f4611bb85259c358cf5e138cce2be8e7bcaddc
Tisha-I <igbatisha@gmail.com>
2022-10-23 23:33:27 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("main").style.opacity = "0.1";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+    document.getElementById("main").style.opacity = "1";
+  }
