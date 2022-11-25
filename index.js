c3e86f03f849f965dd0a54591975f0c407a641b3
Nimat <nimatorisunmibare@gmail.com>
2022-11-25 20:09:43 +0100

    sidebar changes

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById("main").style.marginRight = "0px";
     document.getElementById("main").style.opacity = "0.1";
   }
   
